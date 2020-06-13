d62ac56e4ee933463abe8baa200981e58f89bba9
ER2-1017 <60078003+ER2-1017@users.noreply.github.com>
2020-06-13 17:52:49 +0300

    Add files via upload

---
+++ Harvester.java
@@ -0,0 +1,80 @@
+package com.dune.game.core.units;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.dune.game.core.*;
+import com.dune.game.core.interfaces.Targetable;
+import com.dune.game.core.units.types.UnitType;
+import com.dune.game.core.users_logic.BaseLogic;
+import com.dune.game.screens.utils.Assets;
+
+public class Harvester extends AbstractUnit {
+
+    private BitmapFont fontTank;
+
+    public Harvester(GameController gc) {
+        super(gc);
+        this.textures = Assets.getInstance().getAtlas().findRegion("tankcore").split(64, 64)[0];
+        this.weaponTexture = Assets.getInstance().getAtlas().findRegion("harvester");
+        this.containerCapacity = 10;
+        this.minDstToActiveTarget = 5.0f;
+        this.speed = 120.0f;
+        this.weapon = new Weapon(4.0f, 1);
+        this.hpMax = 500;
+        this.unitType = UnitType.HARVESTER;
+        this.fontTank = Assets.getInstance().getAssetManager().get("fonts/font32.ttf");
+    }
+
+    @Override
+    public void setup(BaseLogic baseLogic, float x, float y) {
+        this.position.set(x, y);
+        this.baseLogic = baseLogic;
+        this.ownerType = baseLogic.getOwnerType();
+        this.hp = this.hpMax;
+        this.destination = new Vector2(position);
+    }
+
+
+    public void updateWeapon(float dt) {
+        weapon.setAngle(rotateTo(weapon.getAngle(), angle, 180.0f, dt));
+        if (gc.getMap().getResourceCount(position) > 0) {
+            int result = weapon.use(dt);
+            if (result > -1) {
+                container += gc.getMap().harvestResource(position, result);
+                if (container > containerCapacity) {
+                    container = containerCapacity;
+                }
+            }
+        } else {
+            weapon.reset();
+        }
+    }
+
+    @Override
+    public void commandAttack(Targetable target) {
+        commandMoveTo(target.getPosition());
+    }
+
+    @Override
+    public void renderGui(SpriteBatch batch) {
+        super.renderGui(batch);
+        fontTank.draw(batch, "Picked: "+String.valueOf(container), position.x + 40, position.y - 40, 10, 0, false);
+        if (weapon.getUsageTimePercentage() > 0.0f) {
+            batch.setColor(0.2f, 0.2f, 0.0f, 1.0f);
+            batch.draw(progressbarTexture, position.x - 32, position.y + 22, 64, 8);
+            batch.setColor(1.0f, 1.0f, 0.0f, 1.0f);
+            batch.draw(progressbarTexture, position.x - 30, position.y + 24, 60 * weapon.getUsageTimePercentage(), 4);
+            batch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+        }
+    }
+
+    public void update(float dt) {
+        super.update(dt);
+        Building b = gc.getMap().getBuildingEntrance(getCellX(), getCellY());
+        if (b != null && b.getType() == Building.Type.STOCK && b.getOwnerLogic() == this.baseLogic) {
+            baseLogic.addMoney(container * 100);
+            container = 0;
+        }
+    }
+}
